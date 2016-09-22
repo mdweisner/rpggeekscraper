0564e33eb25b1c8321a2ff3933ffcfb4ed539c55
mdweisner <michael.d.weisner@gmail.com>
2016-09-22 16:55:26 -0400

    Update get_rpg_info.py

--- get_rpg_info.py
+++ get_rpg_info.py
@@ -12,13 +12,13 @@ def get_val(tag, term):
     	return val
 
 
-base = 'http://www.rpggeek.com/xmlapi2/family?type=rpg&id={}&stats=1&historial=1'
+base = 'http://www.rpggeek.com/xmlapi2/family?typ=rpg&id={}&stats=1'
 with open('ids.txt') as f:
     ids = [line.strip() for line in f.readlines()]
 split = 30
 f = open('rpgs.csv', 'w')
 writer = csv.writer(f)
-writer.writerow(('id', 'type', 'name', 'bggrating', 'users_rated', 'average_rating',
+writer.writerow(('id', 'type', 'name', 'yearpublished', 'rpggeekrank', 'users_rated', 'average_rating',
                  'bayes_average_rating', 'total_owners', 'total_traders', 'total_wanters',
                  'total_wishers', 'total_comments', 'total_weights', 'average_weight'))
 for i in range(0, len(ids), split):
@@ -32,6 +32,7 @@ for i in range(0, len(ids), split):
         gtype = item['type']
         gname = get_val(item, 'name')
         gyear = get_val(item, 'yearpublished')
+        grpggeekrank = get_val(item, 'rpggeekrank')
         usersrated = get_val(item.statistics.ratings, 'usersrated')
         avg = get_val(item.statistics.ratings, 'average')
         bayesavg = get_val(item.statistics.ratings, 'bayesaverage')
@@ -43,8 +44,9 @@ for i in range(0, len(ids), split):
         numweights = get_val(item.statistics.ratings, 'numweights')
         avgweight = get_val(item.statistics.ratings, 'averageweight')
         # desc = item.description.text.encode('ascii', 'ignore')
-        writer.writerow((gid, gtype, gname, gyear,
+        writer.writerow((gid, gtype, gname, gyear, grpggeekrank,
                          usersrated, avg, bayesavg, owners, traders, wanters, wishers, numcomments,
                          numweights, avgweight))
     time.sleep(2)
 f.close()
+
